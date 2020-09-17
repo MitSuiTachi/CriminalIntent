95bc54ec372641dec5cd25177b5242410e5c2096
wanghaoping <wanghaoping@xiaomi.com>
2020-09-17 21:44:37 +0800

    准备把代码提交Github管理，保存当前文件及修改。
    Signed-off-by: wanghaoping <wanghaoping@xiaomi.com>

--- app/src/main/java/com/bignerdranch/android/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/CrimeListFragment.java
@@ -1,9 +1,11 @@
 package com.bignerdranch.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,12 +48,15 @@ public class CrimeListFragment extends Fragment {
         private TextView mTitleTextView;
         private TextView mDateTextView;
 
+        private ImageView mSolvedImageView;
+
         private Crime mCrime;
 
         public CrimeHolder(LayoutInflater inflater,ViewGroup parent){
             super(inflater.inflate(R.layout.list_item_crime,parent,false));
             mTitleTextView = (TextView)itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView)itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
 
             itemView.setOnClickListener(this);
         }
@@ -60,12 +65,16 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),mCrime.getTitle() + "clicked!",Toast.LENGTH_SHORT)
-                    .show();
+//            Toast.makeText(getActivity(),mCrime.getTitle() + "clicked!",Toast.LENGTH_SHORT)
+//                    .show();
+//            Intent intent = new Intent(getActivity(),CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            startActivity(intent);
         }
     }
 
